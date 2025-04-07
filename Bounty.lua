fdd6ace44784b49267d70a96973a3c8ca5768877
x2seed-hub <pocketcode329@gmail.com>
2025-04-07 10:55:29 +0700

    Update and rename README.md to Bounty.lua

---
+++ Bounty.lua
@@ -0,0 +1,70 @@
+getgenv().Config = {
+ ["Team"] = "Marines",
+ ["Safe Health"] = {40, 50},
+ ["Skip"] = { 
+ ["Fruit"] = {["Enabled"] = true,
+ ["Avoid Fruit"] = {
+ "Portal-Portal", 
+ "Kitsune-Kitsune"
+ }
+ },
+ ["Avoid V4"] = true
+ },
+ ["Hunt Method"] = {
+ ["Use Move Predict"] = true,
+ ["Hit and Run"] = true
+ },
+ ["Spam All Skill On V4"] = {
+ ["Enabled"] = true,
+ ["Weapons"] = {"Melee", "Gun", "Sword", "Blox Fruit"}
+ },
+ ["Chat"] = {
+ ["Enabled"] = false,
+ ["Message"] = {"matsune hub bounty"}
+ },
+ ["Custom Y Run"] = {
+ ["Enabled"] = false,
+ ["Y Run"] = 100
+ },
+ ["Misc"] = {
+ ["Auto Store Fruit"] = true,
+ ["Auto Random Fruit"] = true,
+ ["White Screen"] = false,
+ ["Click Delay"] = 0.1,
+ },
+ ["Items"] = {["Use"] = {"Melee", "Gun", "Sword"},
+ ["Melee"] = {
+ ["Enable"] = true,
+ ["Skills"] = {
+ ["Z"] = {["Enable"] = true, ["HoldTime"] = 1.5},
+ ["X"] = {["Enable"] = true, ["HoldTime"] = 0},
+ ["C"] = {["Enable"] = true, ["HoldTime"] = 0}
+ }
+ },
+ ["Blox Fruit"] = {
+ ["Enable"] = false,
+ ["Skills"] = {
+ ["Z"] = {["Enable"] = true, ["HoldTime"] = 1.2},
+ ["X"] = {["Enable"] = true, ["HoldTime"] = 0},
+ ["C"] = {["Enable"] = true, ["HoldTime"] = 0},
+ ["V"] = {["Enable"] = false, ["HoldTime"] = 0},
+ ["F"] = {["Enable"] = false, ["HoldTime"] = 0}
+ }
+ },
+ ["Sword"] = {
+ ["Enable"] = false,
+ ["Skills"] = {
+ ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.1},
+ ["X"] = {["Enable"] = true, ["HoldTime"] = 0.1}
+ } 
+ },
+ ["Gun"] = {
+ ["Enable"] = true, 
+ ["Skills"] = {
+ ["Z"] = {["Enable"] = true, ["HoldTime"] = 0},
+ ["X"] = {["Enable"] = true, ["HoldTime"] = 0}
+ } 
+ }
+ }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealMatsune/Rolls-Rocye/refs/heads/main/b164o774u241n032t141y.lua"))()
